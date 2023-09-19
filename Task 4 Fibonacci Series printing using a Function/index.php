708f1716d89530aee72e0a6148d615259d8f9150
Muhammad Ohee <md.ohee786@gmail.com>
2023-09-19 18:04:19 +0600

    version 4

--- Task 4 Fibonacci Series printing using a Function/index.php
+++ Task 4 Fibonacci Series printing using a Function/index.php
@@ -3,11 +3,17 @@ function fibonacci($number){
     $number-=2;
     $first_number = 0;
     $second_number = 1;
-    echo $first_number;
-    echo "\n";
-    echo $second_number;
-    echo "\n";
+    // echo $first_number;
+    // echo "\n";
+    // echo $second_number;
+    // echo "\n";
     for($i = 0; $i<$number; $i++){
+        if($first_number == 0 && $second_number == 1){
+            echo $first_number;
+            echo "\n";
+            echo $second_number;
+            echo "\n";
+        }
         $third_number = $first_number + $second_number;
         echo $third_number;
         echo "\n";
